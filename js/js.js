aec116a611bc8a10b67d180b31d9ce9805425c5d
Gil Sala <gilsala_88@hotmail.com>
2022-12-05 08:51:10 +0100

    Change radio to checkbox

--- js/js.js
+++ js/js.js
@@ -1,5 +1,37 @@
-function hola(){
-console.log('miau')
+
+const display = document.querySelector('.book-display');
+const buttonPopUp = document.querySelector('.button-popup');
+const formPopUp = document.querySelector('.form-popup')
+
+buttonPopUp.addEventListener('click',()=>{
+formPopUp.style.display ='block';
+
+});  
+
+
+
+
+
+
+function Book (title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = ['yes I read it','I did not read it yet']
+    this.info = function(){
+        return `The ${this.title} by ${this.author}, ${this.pages} pages, ${this.read[0]}`
+    }
+
 }
 
-hola();
+function addBookToLibrary() {
+
+    
+}
+
+let harry= new Book('Harry potter', 'jjkk', '200', 'yes');
+
+let myLibrary = [harry.info()];
+
+
+display.innerHTML = myLibrary
