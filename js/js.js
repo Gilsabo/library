3c9889be44def067d8e83d2c60dc301c03842b23
Gil Sala <gilsala_88@hotmail.com>
2022-12-13 07:39:23 +0100

    Append arguments of the constructor to book-display

--- js/js.js
+++ js/js.js
@@ -1,14 +1,23 @@
 
-const display = document.querySelector('.book-display');
+const displayBook = document.querySelector('.book-display');
 const buttonPopUp = document.querySelector('.button-popup');
-const formPopUp = document.querySelector('.form-popup')
+const formPopUp = document.querySelector('.form-popup');
+const submitButton = document.querySelector('.submit');
+const overlay = document.querySelector('#overlay');
+const titleDiv = document.querySelector('.title');
+const authorDiv = document.querySelector('.author');
+const pagesDiv = document.querySelector('.pages');
+const readDiv = document.querySelector('.status');
 
 buttonPopUp.addEventListener('click',()=>{
-formPopUp.style.display ='block';
-
+    if(formPopUp.style.display ==='none'){
+        formPopUp.style.display ='block';
+        overlay.style.display='block';
+    }else{
+        formPopUp.style.display ='none';
+        overlay.style.display='none';
+    }
 });  
-
-
 
 
 
@@ -17,21 +26,84 @@ function Book (title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
-    this.read = ['yes I read it','I did not read it yet']
+    this.read = read;
     this.info = function(){
-        return `The ${this.title} by ${this.author}, ${this.pages} pages, ${this.read[0]}`
+        return `The ${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`
     }
-
 }
 
-function addBookToLibrary() {
+
+const HarryPotter = new Book('Harry', 'JK Rowling', '44', 'read');
+
+let myLibrary = [HarryPotter.info()];
+
+
+submitButton.addEventListener('click', (e)=>{
+e.preventDefault();
+formPopUp.style.display ='none';
+overlay.style.display='none'
+const title= document.getElementById('title').value;
+const author=document.getElementById('author').value;
+const pages=document.getElementById('pages').value;
+let read = document.getElementById('read');
+if (read.checked===true){
+    read='read';
+}else{
+    read='no read'; 
+}
+
+const booking = new Book (title, author, pages, read);
+
+const divSubTitle =document.createElement('div');
+divSubTitle.classList.add('.subTitle');
+const textSubTitle = document.createTextNode(booking.title);
+divSubTitle.appendChild(textSubTitle);
+displayBook.appendChild(divSubTitle);
+
+const divSubAuthor =document.createElement('div');
+divSubAuthor.classList.add('subAuthor');
+const textSubAuthor = document.createTextNode(booking.author);
+divSubAuthor.appendChild(textSubAuthor);
+displayBook.appendChild(divSubAuthor);
+
+
+const divSubPages =document.createElement('div');
+divSubPages.classList.add('subPages');
+const textSubPages = document.createTextNode(booking.pages);
+divSubPages.appendChild(textSubPages);
+displayBook.appendChild(divSubPages);
+
+
+
+const divSubRead =document.createElement('div');
+divSubRead.classList.add('subRead');
+const textSubRead = document.createTextNode(booking.read);
+divSubRead.appendChild(textSubRead);
+displayBook.appendChild(divSubRead);
+
+
+myLibrary.push(booking);
+
+document.querySelector('form').reset();
+
+
+})
+
+displayBook.textContent= HarryPotter.info();
+
+
+
+
+
+
+/*function addBookToLibrary() {
 
     
-}
-
-let harry= new Book('Harry potter', 'jjkk', '200', 'yes');
-
-let myLibrary = [harry.info()];
-
-
-display.innerHTML = myLibrary
+    const divSubTitle =document.createElement('div')
+    divSubTitle.classList.add('.title')
+    const textSubTitle = document.createTextNode(myLibrary.title)
+    divSubTitle.appendChild(textSubTitle);
+    titleDiv.appendChild(divSubTitle)
+        
+        
+    }*/
